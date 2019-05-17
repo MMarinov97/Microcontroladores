5cb77d93801a4e96078e73d680b9a5519b40bfc5
MMarinov97 <marinovantonov@gmail.com>
2019-05-17 12:26:29 +0200

    Subido P4.5 corregido segun consejos del profesor

--- P4.5.c
+++ P4.5.c
@@ -15,60 +15,72 @@ void config_perifericos();
 /* Interrupcion para el puerto 1 */
 #pragma vector = PORT1_VECTOR;
 __interrupt void PORT1_ISR(){
-    P2OUT |= BIT4;
-    /* Habilitamos el timer
-     * MC_2 -> Modo continuo, cuenta desde 0 hasta el valor maximo de TAR repetidamente (0xFFFF+1) */
-    TACTL|=MC_2;
-    P1IFG &= ~BIT4; /* Limpiamos el flag de interrupcion */
+    if((P1IFG & BIT4) != 0){
+        P2OUT |= BIT4;
+        /* Habilitamos el timer
+         * MC_2 -> Modo continuo, cuenta desde 0 hasta el valor maximo de TAR repetidamente (0xFFFF+1) */
+        TACTL |= MC_2;
+        TACCTL1 &= ~CCIFG; /* Limpiamos el flag de interrupcion */
+        TACCTL1 |= CCIE; /* Habilitamos la interrupcion */
+        TACCR1 = TAR + NUM;
+        P1IFG &= ~BIT4; /* Limpiamos el flag de interrupcion */
+    }
 }
 /* Interrupcion para el puerto 2 */
 #pragma vector = PORT2_VECTOR;
 __interrupt void PORT2_ISR(){
    /* Comprobamos que boton se ha pulsado y conmutamos el LED correspondiente */
-	if((P2IFG & BIT1) != 0){
-		P2OUT |= BIT5;
-		P2IFG &= ~BIT1; /* Limpiamos el flag de interrupcion */
+    if((P2IFG & BIT1) != 0){
+        P2OUT |= BIT5;
+        P2IFG &= ~BIT1; /* Limpiamos el flag de interrupcion */
         /* Habilitamos el timer
          * MC_2 -> Modo continuo, cuenta desde 0 hasta el valor maximo de TAR repetidamente (0xFFFF+1) */
         TACTL|=MC_2;
-	}
-	if((P2IFG & BIT2) != 0){
+        TACCTL1 &= ~CCIFG; /* Limpiamos el flag de interrupcion */
+        TACCTL1 |= CCIE; /* Habilitamos la interrupcion */
+        TACCR1 = TAR + NUM;
+    }
+    if((P2IFG & BIT2) != 0){
         P2OUT |= BIT6;
-		P2IFG &= ~BIT2; /* Limpiamos el flag de interrupcion */
+        P2IFG &= ~BIT2; /* Limpiamos el flag de interrupcion */
         /* Habilitamos el timer
          * MC_2 -> Modo continuo, cuenta desde 0 hasta el valor maximo de TAR repetidamente (0xFFFF+1) */
-        TACTL|=MC_2;		
-	}
-	if((P2IFG & BIT3) != 0){
+        TACTL|=MC_2;
+        TACCTL1 &= ~CCIFG; /* Limpiamos el flag de interrupcion */
+        TACCTL1 |= CCIE; /* Habilitamos la interrupcion */
+        TACCR1 = TAR + NUM;
+    }
+    if((P2IFG & BIT3) != 0){
         P2OUT |= BIT7;
-		P2IFG &= ~ BIT3; /* Limpiamos el flag de interrupcion */
+        P2IFG &= ~ BIT3; /* Limpiamos el flag de interrupcion */
         /* Habilitamos el timer
          * MC_2 -> Modo continuo, cuenta desde 0 hasta el valor maximo de TAR repetidamente (0xFFFF+1) */
-        TACTL|=MC_2;		
-	}
+        TACTL|=MC_2;
+        TACCTL1 &= ~CCIFG; /* Limpiamos el flag de interrupcion */
+        TACCTL1 |= CCIE; /* Habilitamos la interrupcion */
+        TACCR1 = TAR + NUM;
+    }
 }
 /* Interrupcion para el timer */
 #pragma vector = TIMER0_A1_VECTOR
 __interrupt void RTI_TA0CCR12(){
-    static int cont = 1;
+    static int cont = 0;
     switch(TA0IV){ /* Al acceder al registro TAIV  limpiamos los flags de interrupcion.
                     * Es importante hacerlo ya que sino entraria en un bucle infinito y bloquearia
                     * la CPU */
     case(TA0IV_TACCR1):
+            cont++;
+            TA0CCR1 += NUM;
+
             if(cont == 4){
                 P2OUT &=~ (BIT4|BIT5|BIT6|BIT7);/* Apagamos los LEDs */
                 /* Apagamos el temporizador */
                 /* Actualizamos el valor de TACCR1 para la siguiente cuenta */
                 TACTL &=~ MC_2; /* Apagamos el timer ASI!!!!!!!!! */
-                cont = 1;
-            } else {
-                cont = cont + 1;
-                TACTL &=~ MC_2; /* Apagamos el timer ASI!!!!!!!!! */
-                TACCR1 = TAR + NUM; /* Actualizamos el valor de TACCR1 */
-                TACTL|=MC_2; /* Volvemos a poner el timer */
+                cont = 0;
             }
-
     }
+    TA0CCTL0 &= ~CCIFG;
 }
 
 /* MAIN */
@@ -79,16 +91,9 @@ int main(void){
 /* FIN DE MAIN */
 
 void config_perifericos(){
-    WDTCTL =WDTPW + WDTHOLD; /* Parar el timer del watchdog */
+    WDTCTL = WDTPW + WDTHOLD; /* Parar el timer del watchdog */
 
     /* Configuracion de los pulsadores */
-    P1IE |= BIT4; /* Interrupt habilitado para el boton S3 */
-	P1IES |= BIT4; /* Detectamos flanco de bajada */
-    P1IFG &=~ BIT4; /*  P1.4 IFG limpiado */
-
-    P2IE |= (BIT1|BIT2|BIT3); /* Interrupt habilitado para los botones S4, S5 y S6 */
-	P2IES |= (BIT1|BIT2|BIT3); /* Detectamos flanco de bajada */
-    P2IFG &=~ (BIT1|BIT2|BIT3); /* P2.1, P2.2 y P2.3 IFG limpiado */
 
     /* Boton S3 */
     P1DIR &=~BIT4;/* Lo habilitamos como entrada */
@@ -99,6 +104,14 @@ void config_perifericos(){
     P2DIR &=~(BIT1|BIT2|BIT3);
     P2REN |= (BIT1|BIT2|BIT3);
     P2OUT |= (BIT1|BIT2|BIT3);
+    /* Interrupciones Botones */
+    P1IFG &=~ BIT4; /*  P1.4 IFG limpiado */
+    P1IE |= BIT4; /* Interrupt habilitado para el boton S3 */
+    P1IES |= BIT4; /* Detectamos flanco de bajada */
+
+    P2IFG &=~ (BIT1|BIT2|BIT3); /* P2.1, P2.2 y P2.3 IFG limpiado */
+    P2IE |= (BIT1|BIT2|BIT3); /* Interrupt habilitado para los botones S4, S5 y S6 */
+    P2IES |= (BIT1|BIT2|BIT3); /* Detectamos flanco de bajada */
     /* Configuracion de los LEDs */
 
     P2SEL &=~ (BIT6|BIT7);
@@ -108,13 +121,9 @@ void config_perifericos(){
     P2OUT &=~ (BIT4|BIT5|BIT6|BIT7);/* Inicializamos a APAGADO */
 
     /* Configuracion del timer */
-    TA0CCTL1 = CCIE;
-
-    TACCR1 = NUM; /* NUM = Ciclos que tiene que contar el timer */
-
     /* TASSEL_2 -> Fuente del timer es SMCLK
      * ID_3 -> Divisor del timer /8 */
-    TACTL = TASSEL_2 + ID_3 + TAIE;
+    TACTL = TASSEL_2 + ID_3;
 
     /* Habilitamos los interrupts */
     __enable_interrupt();
